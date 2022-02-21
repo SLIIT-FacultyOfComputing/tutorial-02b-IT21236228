974224b9a97057770a295b00aab73769bc49e4f2
IT21236228 <99157987+IT21236228@users.noreply.github.com>
2022-02-21 08:59:01 +0530

    Update tute01.cpp

--- tute01.cpp
+++ tute01.cpp
@@ -22,9 +22,9 @@ printf("Hello World") --> std::cout << "Hello World";
 int main() 
 {
     float cm, inches;
-    printf("Enter a length in cm : ");
-    scanf("%f",&cm);
+    cout<<"Enter a length in cm : ";
+    cin>>cm;
     inches = cm / 2.54;
-    printf("Length in inches is %f \n", inches);
+    cout<<"Length in inches is %f \n", inches;
     return 0;
 }  
